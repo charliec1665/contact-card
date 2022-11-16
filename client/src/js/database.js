6deeddb41985e3823afbf814c7230b593e47cd89
Charlie Carter <charliec1665@gmail.com>
2022-11-16 16:07:30 -0500

    render functionality

---
+++ client/src/js/database.js
@@ -0,0 +1,61 @@
+import { openDB } from "idb";
+import 'regenerator-runtime/runtime';
+
+export const initDb = async () => {
+    // creating a new database named 'contact_db' which will be using version 1 of the database
+    openDB('contact_db', 1, {
+        // Add our database schema if it has not already been initialized
+        upgrade(db) {
+            if (db.objectStoreNames.contains('contacts')) {
+                console.log('contacts store already exists');
+                return;
+            }
+            // create a new object store for the data and give it a key name of 'id' which will increment automatically
+            db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+            console.log('contacts store created');
+        }
+    })
+};
+
+// Export a function we will use to GET to the database
+export const getDb = async () => {
+    console.log('GET from the database');
+
+    // Create a connection to the IndexedDB database and the version we want to use
+    const contactDb = await openDB('contact_db', 1);
+
+    // Create a new transaction and specify the store and data privileges
+    const tx = contactDb.transaction('contacts', 'readonly');
+
+    // Open up the desire object store.
+    const store = tx.objectStore('contacts');
+
+    // Use the .getAll method to get all data in the database
+    const request = store.getAll();
+
+    // Get confirmation of request
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
+
+// Export a function we will use to POST to the database.
+export const postDb = async (name, email, phone, profile) => {
+    console.log('POST to the database');
+  
+    // Create a connection to the database and specify the version we want to use.
+    const contactDb = await openDB('contact_db', 1);
+  
+    // Create a new transaction and specify the store and data privileges.
+    const tx = contactDb.transaction('contacts', 'readwrite');
+  
+    // Open up the desired object store.
+    const store = tx.objectStore('contacts');
+  
+    // Use the .add() method on the store and pass in the content.
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+  
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,22 +1,66 @@
 // Entry Point
 
+// Import indexedDb
+import { initDb, getDb, postDb } from './database';
+
+import { toggleForm, clearForm } from './form';
+
 // Import modules
 import ('./form');
-import ('./submit');
 
 // Import CSS
-import "../css/index.css";
+import '../css/index.css';
 
 // Import Bootstrap
 import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { fetchCards } from './cards';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function() {
+    initDb();
+    fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
+});
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+  
+newContactButton.addEventListener('click', event => {
+    toggleForm()
+})
+  
+form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+    if (submitBtnToUpdate == false) {
+        postDb(name, email, phone, profile);
+    } else {
+  
+        fetchCards();
+        // Toggles the submit button back to POST functionality
+        submitBtnToUpdate = false;
+    }
+  
+    // Clear form
+    clearForm();
+    // Toggle form
+    toggleForm();
+    // Reload the DOM
+    fetchCards();
 });
