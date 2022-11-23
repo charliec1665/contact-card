c576977b83f95bee94197d7ef6f952de02295038
Charlie Carter <charliec1665@gmail.com>
2022-11-23 18:26:31 -0500

    added install button

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -16,6 +16,9 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// grabbing install button
+const installBtn = document.getElementById('installBtn');
+
 // On load functionality
 if ('serviceWorker' in navigator) {
     // Use the window load event to keep the page load performant
@@ -30,6 +33,21 @@ window.addEventListener('load', function() {
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
+});
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+    
+    installBtn.addEventListener('click', () => {
+        event.prompt();
+        installBtn.setAttribute('disabled', true);
+        installBtn.textContent = 'Installed!';
+    });
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
 });
 
 
